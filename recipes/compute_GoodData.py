5f1ff55a31a7357a6828df721c65d5c6e7381651
rayan <rayan>
2021-08-13 13:29:24 +0000

    Recipe LOGSANALYSIS.compute_GoodData saved through notebook notebook editor for compute_GoodData

--- recipes/compute_GoodData.py
+++ recipes/compute_GoodData.py
@@ -1,13 +1,15 @@
-# -*- coding: utf-8 -*-
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 import dataiku
 import pandas as pd, numpy as np
 from dataiku import pandasutils as pdu
 
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 # Read recipe inputs
 prepared = dataiku.Dataset("prepared")
-prepared_df = prepared.get_dataframe()
+df = prepared.get_dataframe()
+df.head()
 
-
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 # Compute recipe outputs from inputs
 # TODO: Replace this part by your actual code that computes the output, as a Pandas dataframe
 # NB: DSS also supports other kinds of APIs for reading and writing data. Please see doc.
