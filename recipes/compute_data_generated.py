20330a9d94b7838aa4aedde22fbd42c0ad36bdea
tony <tony>
2021-08-24 16:21:21 +0000

    Recipe LOGSANALYSIS.compute_data_generated saved through notebook notebook editor for compute_data_generated

--- recipes/compute_data_generated.py
+++ recipes/compute_data_generated.py
@@ -1,10 +1,36 @@
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 # -*- coding: utf-8 -*-
 import dataiku
 import pandas as pd, numpy as np
 from dataiku import pandasutils as pdu
 
 
+# Read recipe inputs
+json_prepared = dataiku.Dataset("Json_prepared")
+df = json_prepared.get_dataframe()
 
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+from datetime import timedelta
+import random
+
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+max_index = len(df)-10
+random_index = random.randint(0,max_index)
+
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+df_generated = df[random_index:random_index+10]
+
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+actualdate = datetime
+print(lastdate)
+for i in range(len(val)):
+    lastdate = lastdate + timedelta(minutes=15)
+    df.append()
+
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+df_generated
+
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 # Compute recipe outputs
 # TODO: Write here your actual code that computes the outputs
 # NB: DSS supports several kinds of APIs for reading and writing data. Please see doc.
