dadd1d261d6afcdeab75a1a168b2184afad03219
tony <tony>
2021-08-31 13:37:22 +0000

    Recipe LOGSANALYSIS.compute_dHoUQGRB saved through notebook notebook editor for compute_dHoUQGRB

--- recipes/compute_dHoUQGRB.py
+++ recipes/compute_dHoUQGRB.py
@@ -66,22 +66,22 @@ for i in test_df.columns:
 
 # -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 def split_series(series, n_past, n_future):
-  #
-  # n_past ==> no of past observations
-  #
-  # n_future ==> no of future observations
-  #
-  X, y = list(), list()
-  for window_start in range(len(series)):
-    past_end = window_start + n_past
-    future_end = past_end + n_future
-    if future_end > len(series):
-      break
-    # slicing the past and future parts of the window
-    past, future = series[window_start:past_end, :], series[past_end:future_end, :]
-    X.append(past)
-    y.append(future)
-  return np.array(X), np.array(y)
+    #
+    # n_past ==> no of past observations
+    #
+    # n_future ==> no of future observations
+    #
+    X, y = list(), list()
+    for window_start in range(len(series)):
+        past_end = window_start + n_past
+        future_end = past_end + n_future
+        if future_end > len(series):
+            break
+        # slicing the past and future parts of the window
+        past, future = series[window_start:past_end, :], series[past_end:future_end, :]
+        X.append(past)
+        y.append(future)
+    return np.array(X), np.array(y)
 
 # -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 n_past = 10
